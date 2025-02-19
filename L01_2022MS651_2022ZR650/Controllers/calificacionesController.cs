991cf0aa2ba63566e16bfa375d60ef820fd7b0a9
Ever Zamora <ever.zamora@catolica.edu.sv>
2025-02-18 18:56:04 -0600

    Controladores: Comentarios y calificaciones

--- L01_2022MS651_2022ZR650/Controllers/calificacionesController.cs
+++ L01_2022MS651_2022ZR650/Controllers/calificacionesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_2022MS651_2022ZR650.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2022MS651_2022ZR650.Controllers
 {
@@ -7,5 +9,93 @@ namespace L01_2022MS651_2022ZR650.Controllers
     [ApiController]
     public class calificacionesController : ControllerBase
     {
+        private readonly blogContext _blogContext;
+
+        [HttpGet]
+        [Route("ObtenerCalificaciones")]
+        public IActionResult Get()
+        {
+            List<Calificaciones> listadoCalificaciones = (from a in _blogContext.calificaciones select a).ToList();
+            if (listadoCalificaciones.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoCalificaciones);
+        }
+
+        [HttpGet]
+        [Route("ObtenerCalificacionesByPublicacion/{id}")]
+        public IActionResult ObtenerCalificacionesByPublicacion(int id)
+        {
+            List<Calificaciones> listadoCalificaciones = (from a in _blogContext.calificaciones 
+                                                          where a.publicacionId == id 
+                                                          select a).ToList();
+            if (listadoCalificaciones.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoCalificaciones);
+        }
+
+        [HttpPost]
+        [Route("AgregarCalificaciones")]
+        public IActionResult guardarCalificacion([FromBody] Calificaciones calificacion)
+        {
+            try
+            {
+                _blogContext.calificaciones.Add(calificacion);
+                _blogContext.SaveChanges();
+                return Ok(calificacion);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("actualizarCalificacion/{id}")]
+        public IActionResult ActualizarCalificacion(int id, [FromBody] Calificaciones calificacionModificar)
+        {
+            Calificaciones? calificacionActual = (from a in _blogContext.calificaciones
+                                                  where a.calificacionId == id
+                                                  select a).FirstOrDefault();
+
+            if (calificacionActual == null)
+            {
+                return NotFound();
+            }
+            calificacionActual.publicacionId = calificacionModificar.publicacionId;
+            calificacionActual.usuarioId = calificacionModificar.usuarioId;
+            calificacionActual.calificacion = calificacionModificar.calificacion;
+
+
+            _blogContext.Entry(calificacionActual).State = EntityState.Modified;
+            _blogContext.SaveChanges();
+
+            return Ok(calificacionModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminarCalificacion/{id}")]
+        public IActionResult EliminarCalificacion(int id)
+        {
+            Calificaciones? calificacion = (from a in _blogContext.calificaciones
+                            where a.calificacionId == id
+                            select a).FirstOrDefault();
+
+            if (calificacion == null)
+            {
+                return NotFound();
+            }
+
+            _blogContext.calificaciones.Attach(calificacion);
+            _blogContext.calificaciones.Remove(calificacion);
+            _blogContext.SaveChanges();
+
+            return Ok(calificacion);
+        }
+
     }
 }
--- L01_2022MS651_2022ZR650/Controllers/comentariosController.cs
+++ L01_2022MS651_2022ZR650/Controllers/comentariosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_2022MS651_2022ZR650.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2022MS651_2022ZR650.Controllers
 {
@@ -7,5 +9,92 @@ namespace L01_2022MS651_2022ZR650.Controllers
     [ApiController]
     public class comentariosController : ControllerBase
     {
+        private readonly blogContext _blogContext;
+
+        [HttpGet]
+        [Route("ObtenerComentarios")]
+        public IActionResult ObtenerComentarios()
+        {
+            List<Comentarios> listadoComentarios = (from a in _blogContext.comentarios select a).ToList();
+            if (listadoComentarios.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoComentarios);
+        }
+
+        [HttpGet]
+        [Route("ObtenerComentariosByUser/{id}")]
+        public IActionResult ObtenerComentariosByUser(int id)
+        {
+            List<Comentarios> listadoComents = (from a in _blogContext.comentarios
+                                                          where a.usuarioId == id
+                                                          select a).ToList();
+            if (listadoComents.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoComents);
+        }
+
+        [HttpPost]
+        [Route("AgregarComentarios")]
+        public IActionResult guardarComentarios([FromBody] Comentarios comentario)
+        {
+            try
+            {
+                _blogContext.comentarios.Add(comentario);
+                _blogContext.SaveChanges();
+                return Ok(comentario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("actualizarComentario/{id}")]
+        public IActionResult ActualizarCoementario(int id, [FromBody] Comentarios comentarioModificar)
+        {
+            Comentarios? comentarioActual = (from a in _blogContext.comentarios
+                                                  where a.cometarioId == id
+                                                  select a).FirstOrDefault();
+
+            if (comentarioActual == null)
+            {
+                return NotFound();
+            }
+            comentarioActual.publicacionId = comentarioModificar.publicacionId;
+            comentarioActual.usuarioId = comentarioModificar.usuarioId;
+            comentarioActual.comentario = comentarioModificar.comentario;
+
+
+            _blogContext.Entry(comentarioActual).State = EntityState.Modified;
+            _blogContext.SaveChanges();
+
+            return Ok(comentarioModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminarComentario/{id}")]
+        public IActionResult EliminarComentario(int id)
+        {
+            Comentarios? comentario = (from a in _blogContext.comentarios
+                                            where a.cometarioId == id
+                                            select a).FirstOrDefault();
+
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            _blogContext.comentarios.Attach(comentario);
+            _blogContext.comentarios.Remove(comentario);
+            _blogContext.SaveChanges();
+
+            return Ok(comentario);
+        }
     }
 }
